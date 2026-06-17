b08aefde62c801a4e3ca567282e3a14c6ec09ff9
IvqryDev <noah7365@outlook.com>
2026-06-16 21:15:20 -0500

    Load config on effect application.

--- src/main/java/net/ivqrydev/crumbling_kunai/effect/CrumblingEffect.java
+++ src/main/java/net/ivqrydev/crumbling_kunai/effect/CrumblingEffect.java
@@ -1,11 +1,13 @@
 package net.ivqrydev.crumbling_kunai.effect;
 
 import net.ivqrydev.crumbling_kunai.config.ModConfig;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
+import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.resources.ResourceLocation;
 
 public class CrumblingEffect extends MobEffect {
 
@@ -14,11 +16,26 @@ public class CrumblingEffect extends MobEffect {
 
     public CrumblingEffect(MobEffectCategory category, int color) {
         super(category, color);
-        this.addAttributeModifier(
-                Attributes.ARMOR,
-                MODIFIER_ID,
-                -ModConfig.ARMOR_REDUCTION.get(),
-                AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL
-        );
+    }
+
+    @Override
+    public void addAttributeModifiers(AttributeMap attributeMap, int amplifier) {
+        AttributeInstance instance = attributeMap.getInstance(Attributes.ARMOR);
+        if (instance != null) {
+            instance.removeModifier(MODIFIER_ID);
+            instance.addOrReplacePermanentModifier(new AttributeModifier(
+                    MODIFIER_ID,
+                    -ModConfig.ARMOR_REDUCTION.get(),
+                    AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL
+            ));
+        }
+    }
+
+    @Override
+    public void removeAttributeModifiers(AttributeMap attributeMap) {
+        AttributeInstance instance = attributeMap.getInstance(Attributes.ARMOR);
+        if (instance != null) {
+            instance.removeModifier(MODIFIER_ID);
+        }
     }
 }
